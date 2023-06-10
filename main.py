bdab0a21aea29d471b5300f4d67f794adc8a987e
KoHTpyx <trukhanov.kostik@gmail.com>
2023-06-10 19:58:19 +0200

    Update main.py to import handle_error and handle errors

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import sys
 from maze_generator import get_maze_size, get_maze
 from maze_solver import get_start_position, shortest_path, print_directions, handle_error
 
 def main():
     rows, cols = get_maze_size()
     maze = get_maze(rows, cols)
-    start = get_start_position(rows, cols)
+    start = get_start_position(maze, rows, cols)
 
     global error_count
     error_count = 0
